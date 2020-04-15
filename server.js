4992ad0bf424e7d3c3c9c62d70c9ef7dc16a1be2
Filippo Varini <filippovarini@filippos-mbp.home>
2020-04-15 08:04:53 +0200

    raw (hoping)

--- server.js
+++ server.js
@@ -40,8 +40,7 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    // res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
-    res.sendFile(path.resolve(__dirname, "shit", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
   });
 }
 
