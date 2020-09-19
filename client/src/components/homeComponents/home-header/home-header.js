e03358b870e6fcbeb65a188f544be405d83e4512
Filippo Varini <filippovarini@filippos-mbp.home>
2020-09-19 03:18:11 +0200

    fucking hoping

--- client/src/components/homeComponents/home-header/home-header.js
+++ client/src/components/homeComponents/home-header/home-header.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-import "react-slidedown/lib/slidedown.css";
 import "./home-header.css";
 
 import imageSrc from "../../../images/logo-black.png";
