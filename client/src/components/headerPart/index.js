b9a0c20984768b0ea6a76d82c958b5d3ab5def89
Filippo Varini <filippovarini@filippos-mbp.home>
2020-09-21 21:46:39 +0200

    bug fix

--- client/src/components/headerPart/index.js
+++ client/src/components/headerPart/index.js
@@ -4,9 +4,7 @@ import "./HeaderPart.css";
 
 const HeaderPart = ({ title }) => {
   return (
-    // <div id={`${mainClass}-image-container`}>
     <div id="hi">
-      {/* <p id={`${headerClass}fake-header`}>{title}</p> */}
       <p id="hi-title">{title}</p>
       <img id="hi-image" src={mainLogo} alt="logo" />
     </div>
