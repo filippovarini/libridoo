c89839e7c536750bbdb6b6bdd6589b0094337d51
Filippo Varini <filippovarini@Filippos-MBP.homenet.telecomitalia.it>
2020-09-24 22:46:56 +0200

    stripe complete

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -3,5 +3,6 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET,
   S3_KEY_ID: process.env.S3_KEY_ID,
   S3_SECRET_KEY: process.env.S3_SECRET_KEY,
-  EMAIL_PASS: process.env.EMAIL_PASS
+  EMAIL_PASS: process.env.EMAIL_PASS,
+  STRIPE_SECRET: process.env.STRIPE_SECRET
 };
