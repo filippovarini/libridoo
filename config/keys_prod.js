cc6a683e255abea1c057bb935a619f8b6cebf963
Filippo Varini <filippovarini@Filippos-MBP.homenet.telecomitalia.it>
2020-09-25 02:06:37 +0200

    paypal problem fix

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -4,5 +4,7 @@ module.exports = {
   S3_KEY_ID: process.env.S3_KEY_ID,
   S3_SECRET_KEY: process.env.S3_SECRET_KEY,
   EMAIL_PASS: process.env.EMAIL_PASS,
-  STRIPE_SECRET: process.env.STRIPE_SECRET
+  STRIPE_SECRET: process.env.STRIPE_SECRET,
+  PAYPAL_ID: process.env.PAYPAL_ID,
+  PAYPAL_SECRET: process.env.PAYPAL_SECRET
 };
